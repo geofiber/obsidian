96007f5ffcd1800cb07d6e0c9a85a10cc5f36c1f
Richard Scalzo <richard.scalzo@sydney.edu.au>
2018-05-15 10:42:19 +1000

    Ripped out autoshape and gp_predict since we have proper field obs fwd models

--- src/python-utils/diagplots.py
+++ src/python-utils/diagplots.py
@@ -17,30 +17,6 @@ import GPy
 runtag = 'gascoyne_v3'
 
 
-def autoshape(sensors):
-    """
-    Senses the shape of a flattened coordinate array by looking for the
-    index at which coordinates start to repeat.  matplotlib contour maps
-    have to have (x, y) in regular grid inputs, which is really annoying
-    because the sensor grids are provided in terms of just (x, y) lists.
-    :param sensors:  np.array of shape (N, 3) with the physical
-        coordinates (x,y,z) of the sensors in meters; assumed to have a
-        regular raster-like structure where N factors into (Nx, Ny)
-    :return:  inferred shape (x, y) of coordinate array
-    """
-    # Take differences between successive oordinates in the list
-    x, y, z = sensors.T
-    mask_x = (np.abs(x[1:] - x[:-1]) > 1e-3)
-    mask_y = (np.abs(y[1:] - y[:-1]) > 1e-3)
-    # Find the minimum index at which a difference is found
-    nx = np.min(np.arange(len(mask_x))[mask_x]) + 1
-    ny = np.min(np.arange(len(mask_y))[mask_y]) + 1
-    # Use whichever of these is nontrivial to reshape the list
-    if nx == 1:
-        return (-1, ny)
-    elif ny == 1:
-        return (nx, -1)
-
 def plot_sensor(sensors, readings, chain, sample=None, units='unknown units'):
     """
     Plots the value of a sensor against the forward model:
@@ -91,68 +67,6 @@ def plot_sensor(sensors, readings, chain, sample=None, units='unknown units'):
     plt.xlabel("Data $-$ Model ({})".format(units))
     # Show
     plt.show()
-
-def gp_predict(sensors, layer_pars, bounds):
-    """
-    Bare-bones GP predictor for layer depths at a grid of locations.
-    ...ugh I'm not going to finish that today.  :/
-    :param sensors:  np.array of shape (N, 3) with the physical
-        coordinates (x,y,z) of the sensors in meters
-    :param layer_pars:  np.array of shape (Nl, Nx, Ny) containing the
-        layer heights at the control points
-    :param bounds:  np.array of layer bounds
-    """
-    # Boundaries of the region
-    xmin, Lx = bounds[0][0], bounds[0][1] - bounds[0][0]
-    ymin, Ly = bounds[1][0], bounds[1][1] - bounds[1][0]
-    Xpred = sensors[:,:-1]
-
-    gp_pred_list = [ ]
-    for l, pars in enumerate(layer_pars):
-        # Figure out all these auto-magical lengthscales
-        nx, ny = pars.shape
-        xLS = 0.5 * Lx/(nx - 0.99999)
-        yLS = 0.5 * Ly/(ny - 0.99999)
-        xvals = xmin + Lx*np.arange(nx)/(nx - 0.99999)
-        yvals = ymin + Ly*np.arange(ny)/(ny - 0.99999)
-        xg, yg = np.meshgrid(xvals, yvals)
-        # Set up and fit a shitty GP, and add it to the stack
-        k = GPy.kern.RBF(input_dim=2,
-                         lengthscale=(xLS, yLS), ARD=True)
-        X = np.vstack([xg.ravel(), yg.ravel()]).T
-        Y = pars.reshape(-1,1)
-        gp = GPy.models.GPRegression(X[:], Y, kernel=k)
-        gp.Gaussian_noise.variance = 0.001
-        gpmu, gpsig = gp.predict(Xpred)
-        # gp.plot()
-        # plt.show()
-        gp_pred_list.append(gpmu)
-
-    # Read out the formation at the surface
-    gpz = np.array(gp_pred_list).reshape(len(layer_pars), len(Xpred))
-    print "gpz.shape =", gpz.shape
-    print "gpz =", gpz
-    synthform = np.zeros(len(Xpred))
-    for i, xy in enumerate(Xpred):
-        for l in range(len(layer_pars)):
-            if gpz[l,i] > 0:
-                synthform[i] = l
-                break
-    print "synthform =", synthform
-    
-    # Make a contour plot, because why not
-    gridshape = autoshape(sensors)
-    x, y = Xpred.T
-    xg, yg = Xpred.reshape(2, *gridshape)
-    zg = synthform.reshape(*gridshape)
-    # plt.contourf(xg, yg, zg, alpha=0.5)
-    # plt.colorbar()
-    print "x =", x
-    print "y =", y
-    for l in range(len(layer_pars)):
-        idx = (synthform == l)
-        plt.plot(x[idx], y[idx], label='layer {}'.format(l), ls='None', marker='o')
-    plt.legend()
 
 def display_ground_truth(labels, spherical=True):
     """
